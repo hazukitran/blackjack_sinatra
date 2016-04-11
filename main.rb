37e1024a19c65464c0344b4f2beeed0394631c1b
Lexi Tran <hazukitran@gmail.com>
2016-04-11 16:53:37 +0700

    Deal cards and calculate total

--- main.rb
+++ main.rb
@@ -5,15 +5,57 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :path => '/',
                            :secret => 'hazukitran'
 
+helpers do
+  def calculate_total(cards)
+    arr = cards.map { |element| element[1] }
+
+    total = 0
+    arr.each do |a|
+      if a == "A"
+        total += 11
+      else
+        total += a.to_i == 0 ? 10 : a.to_i
+      end
+    end
+
+    arr.select { |element| element == "A" }.count.times do
+      break if total <= 21
+      total -= 10
+    end
+
+    total
+  end
+end
+
 get '/' do
-  erb :set_name
+  if session[:player_name]
+    redirect '/game'
+  else
+    redirect '/new_player'
+  end
 end
 
-post '/set_name' do
+get '/new_player' do
+  erb :new_player
+end
+
+post '/new_player' do
   session[:player_name] = params[:player_name]
-  redirect "/game"
+  redirect '/game'
 end
 
-get "/game" do
+get '/game' do
+  suits = ['H', 'D', 'C', 'S']
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle!
+
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+
   erb :game
 end
